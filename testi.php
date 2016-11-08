d6cd826dba922b420f95466eac6e1a6f8d01f520
Jani Keskiaho <t4keja01@students.oamk.fi>
2016-11-08 09:39:30 +0200

    Made get_post and post_esim

---
+++ testi.php
@@ -0,0 +1,19 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>Testi sivu</title>
+</head>
+<body>
+
+<h1>Testataan PHP:ta</h1>
+
+<?php
+$x=5;
+echo 'Muuttujan arvo on '.$x;
+echo '<br>';
+echo "Muuttujan arvo on $x";
+<p>This is a paragraph.</p>
+?>
+
+</body>
+</html> 
